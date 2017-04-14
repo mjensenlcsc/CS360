6d885e5c09c3483999dbd48ce0fc4d1966d67058
Mikel Jensen <mrjensen@lcmail.lcsc.edu>
2017-04-14 10:25:30 -0700

    saved something?:

--- app.js
+++ app.js
@@ -31,8 +31,10 @@ server.listen(Number(process.env.PORT || "5000"), function() {
 
 app.use(function(req, res, next) {
 
+	console.info(req.hostname)
+
 	// Skip checks if we're running local.
-	if (req.hostname == "localhost")
+	if (req.hostname == "localhost" || req.hostname == 'cs360.herokuapp.com')
 	{
 		next();
 		return;
