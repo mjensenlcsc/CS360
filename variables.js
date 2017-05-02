d79d859338c7f773d49602de98a55b11b6ae1a09
Mikel Jensen <mrjensen@lcmail.lcsc.edu>
2017-05-02 09:52:53 -0700

    Generate a schedule

--- variables.js
+++ variables.js
@@ -2,6 +2,6 @@ const variables = {
 	teams: {},
 	games: {},
 	weeks: {},
-	teamNames: ['One', 'Two', 'Three', 'Four', 'Five']
+	teamNames: ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
 }
 module.exports = variables;
